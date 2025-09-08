bad8495f5b023de06320a54620c6c8f4df2f87f7
Shivaansh Gusain <shivaansh.gusain@gmail.com>
2025-09-08 20:21:55 +0530

    Fixed an import command

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from core.config import settings
+from config import settings
 
 # Import routers from api folder
 from api import alerts, geofence, location
